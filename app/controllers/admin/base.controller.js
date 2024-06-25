e6331d3a5119fd3a32310d2424c09903e2551438
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-06-25 13:37:19 +0530

    Init

--- app/controllers/admin/base.controller.js
+++ app/controllers/admin/base.controller.js
@@ -1,3 +1,4 @@
+//testing lazyg
 const express = require('express');
 const router = express.Router();
 
--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -1,7 +1,28 @@
 //const jwt = require('jsonwebtoken');
 module.exports = function auth(authKeyword) { return function auth(req, res, next){
 
-    const token = req.header('x-auth-token');
+    const token = req.header('x-auth-token'); 
+
+
+
+    // Check token availibility
+    // If not -> return Error or Redirect to Login or call return method
+    
+    // If yes -> Check DB for session Token & Verify User Agent
+    
+    // If Token is not present in DB -> return Error or Redirect to Login or call return method
+    
+    // Check for token Expiry : If Expired -> return Error or Redirect to Login or call return method
+    
+    // Here the token is valid and in DB
+    
+    // getUser() with permission String 
+
+    // Check for inactive user or user locking 
+    
+    // If Token is valid -> Next() and Slide User Expiry Time
+
+
    // if(!token) res.status(401).send('Access Denied. No Token Provided.');
     req.user = {"name":"Mandeep Saini","_id":"12345"};
     req.authKeyword = authKeyword;
